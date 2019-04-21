e22bbd5945ca119d3800a911df2bbc10c4004fa6
Renaldo Cavalcante <renaldocavalcantejr@gmail.com>
2019-04-21 18:46:15 -0300

    Exploratory Data Analysis Done

--- project_modules.py
+++ project_modules.py
@@ -9,4 +9,22 @@ def series_to_list(data,columns=None):
 
     return list(data[columns].values)
 
-        
+def find_outliers_df(data,method="std"):
+    if method == 'std':
+        outliers = dict()
+        for column in data.columns:
+            outliers[column] = data[data[column].std > 3]    
+    return outliers
+
+
+def find_outliers_series(series):
+    outliers = series[series>(series.std()*3)]
+
+    return outliers
+
+
+
+
+
+
+
